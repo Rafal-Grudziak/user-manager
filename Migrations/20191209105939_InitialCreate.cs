b4efd0a5eada1ab22088fde0b1d16160fff1a785
Rafal-Grudziak <grudziakr@gmail.com>
2019-12-10 10:00:17 +0100

    Initial commit

---
+++ Migrations/20191209105939_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace users.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    u_Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    u_Type = table.Column<int>(nullable: false),
+                    U_Active = table.Column<bool>(nullable: false),
+                    U_Code = table.Column<string>(type: "varchar(60)", nullable: false),
+                    u_ForeignId = table.Column<int>(nullable: false),
+                    u_ForeignCode = table.Column<string>(type: "varchar(60)", nullable: true),
+                    u_Name = table.Column<string>(type: "varchar(100)", nullable: true),
+                    u_Pwd = table.Column<string>(type: "varchar(60)", nullable: false),
+                    u_MachineID = table.Column<int>(nullable: false),
+                    u_DefaultLanguage = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.u_Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace users.Models
+{
+    public class User
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int u_Id { get; set; }
+        [Required]
+        [DisplayName("Typ")]
+        public byte u_Type { get; set; }
+        [DisplayName("Czy jest aktywny?")]
+        public bool u_Active { get; set; }
+        [Column(TypeName ="varchar(60)")]
+        [Required]
+        [DisplayName("Login")]
+        public string u_Code { get; set; }
+        [DisplayName("Id z systemu ERP")]
+        public int? u_ForeignId { get; set; }
+        [Column(TypeName = "varchar(60)")]
+        [DisplayName("Nazwa z systemu ERP")]
+        public string u_ForeignCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Imię i nazwisko")]
+        public string u_Name { get; set; }
+        [Column(TypeName = "varchar(60)")]
+        [Required]
+        [DisplayName("Hasło")]
+        public string u_Pwd { get; set; }
+        [DisplayName("Identyfikator maszyny")]
+        public int? u_MachineID { get; set; }
+        [DisplayName("Język")]
+        public byte u_DefaultLanguage { get; set; }
+    }
+}
